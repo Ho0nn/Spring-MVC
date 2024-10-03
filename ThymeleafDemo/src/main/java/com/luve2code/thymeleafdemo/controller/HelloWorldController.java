8e4af8c9c2a21a3dbc6bd9fdea6f3030788520d0
HoOnn <148750499+Ho0nn@users.noreply.github.com>
2024-10-04 01:07:20 +0300

    GitMapping & PostMapping

--- ThymeleafDemo/src/main/java/com/luve2code/thymeleafdemo/controller/HelloWorldController.java
+++ ThymeleafDemo/src/main/java/com/luve2code/thymeleafdemo/controller/HelloWorldController.java
@@ -3,13 +3,16 @@ package com.luve2code.thymeleafdemo.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
 
     // controller method to show initial form
-   @RequestMapping("/showForm")
+   @GetMapping("/showForm")
     public String showForm() {
         return "helloworld-form";
     }
@@ -37,8 +40,20 @@ public class HelloWorldController {
 
        return "helloworld";
      }
+    @RequestMapping("/processFormV3")
+    public String letsShouDudeV3(@RequestParam("studentName") String name ,Model model) {
 
 
+        // convert data to Uppercase
+        name=name.toUpperCase();
 
+        // create a message
+        String res= "Hey My Friend from v3 ! "+name;
+
+        // add message to the model
+        model.addAttribute("message",res);
+
+        return "helloworld";
+    }
 
 }
