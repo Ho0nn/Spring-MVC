f3063cd7998bd316b925d080418480168936237b
HoOnn <148750499+Ho0nn@users.noreply.github.com>
2024-10-05 00:58:54 +0300

    Validation is on fire 🎉

---
+++ ValidationDemo/src/main/java/com/luve2code/springdemo/mvc/controller/CustomerController.java
@@ -0,0 +1,39 @@
+package com.luve2code.springdemo.mvc.controller;
+
+import com.luve2code.springdemo.mvc.model.Customer;
+import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class CustomerController {
+    // add an initbinider... to convert input strings
+    @InitBinder
+    public void initBinder(WebDataBinder webDataBinder){
+        StringTrimmerEditor stringTrimmerEditor=new StringTrimmerEditor(true);
+        webDataBinder.registerCustomEditor(String.class,stringTrimmerEditor);
+    }
+
+
+    @GetMapping("/")
+    public String showForm(Model model){
+        model.addAttribute("customer", new Customer());
+        return "customer-form";
+    }
+    @PostMapping("/processForm")
+    public String processForm(
+            @Valid @ModelAttribute("customer") Customer customer,
+            BindingResult bindingResult) {
+        System.out.println("Last name: |  "+ customer.getLastName() +" |");
+        if (bindingResult.hasErrors())return "customer-form";
+        else return "customer-confirmation";
+    }
+
+}
